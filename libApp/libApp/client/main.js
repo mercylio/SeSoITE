6081ef7ff895c570228c8f3a3e8e370865a17b8d
astan <AlexandruCristian.Stan@stud-inf.unibz.it>
2016-11-14 01:08:12 +0100

    6

--- libApp/libApp/client/main.js
+++ libApp/libApp/client/main.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { Session } from 'meteor/session'
+//import { Session } from 'meteor/session'
+
+Books = new Mongo.Collection("books");
 
 import './main.html';
 
@@ -12,8 +14,67 @@ Accounts.ui.config({
 	passwordSignupFields: "USERNAME_AND_EMAIL"
 });
 
-Template.books.helpers({
-  books:function(){
-    
+//Infinite Scroll
+Session.set("bookListLimit", 10);
+lastScrollTop = 0;
+$(window).scroll(function(event){
+  //test if we're near the bottom of the window
+  if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+    //where are we?
+    var scrollTop = $(this).scrollTop();
+    //test if we are going down
+    if(scrollTop > lastScrollTop){
+      //yes we're going down
+      Session.set("bookListLimit", Session.get("bookListLimit") + 4);
+    }
+    lastScrollTop = scrollTop;
+
   }
 });
+
+//BOOKS
+Template.books.rendered = function() {
+    Session.setDefault('books', [
+      {title: "To Kill a Mockingbird", value: "To Kill a Mockingbird", author: "Harper Lee"},
+      {title: "1984", value:  "1984", author: "George Orwell"},
+      {title: "The Lord of the Rings", value: "The Lord of the Rings", author: "J. R. R. Tolkien"},
+      {title: "The Catcher in the Rye", value: "The Catcher in the Rye", author: "J. D. Salinger"},
+      {title: "The Great Gatsby", value: "The Great Gatsby", author: "F. Scott Fitzgerald"}
+    ]);
+  };
+
+Template.books.helpers({
+  books:function(){
+    return Session.get('books');
+  }
+});
+
+
+//SEARCH
+Template.search.events({
+  "submit #search": function (e) {
+      e.preventDefault();
+      Session.set("searchValue", $("#searchValue").val());
+    }
+});
+
+Template.search.helpers({
+  books: function() {
+    Meteor.subscribe("search", Session.get("searchValue"));
+    if (Session.get("searchValue")) {
+        return Books.find({}, { sort: [["score", "desc"]] });
+      } else {
+        return Books.find({});
+      }
+  }
+});
+
+function seed() {
+  if(!Books.findOne({})) {
+    Books.insert({title: "To Kill a Mockingbird", value: "To Kill a Mockingbird", author: "Harper Lee"});
+      Books.insert({title: "1984", value:  "1984", author: "George Orwell"});
+      Books.insert({title: "The Lord of the Rings", value: "The Lord of the Rings", author: "J. R. R. Tolkien"});
+      Books.insert({title: "The Catcher in the Rye", value: "The Catcher in the Rye", author: "J. D. Salinger"});
+      Books.insert({title: "The Great Gatsby", value: "The Great Gatsby", author: "F. Scott Fitzgerald"});
+  }
+}
--- libApp/libApp/server/main.js
+++ libApp/libApp/server/main.js
@@ -1,5 +1,42 @@
 import { Meteor } from 'meteor/meteor';
+Books = new Mongo.Collection("books");
 
-Meteor.startup(() => {
+Meteor.startup( function() {
   // code to run on server at startup
+
+  function seed() {
+	if(!Books.findOne({})) {
+		Books.insert({title: "To Kill a Mockingbird", value: "To Kill a Mockingbird", author: "Harper Lee"});
+    	Books.insert({title: "1984", value:  "1984", author: "George Orwell"});
+    	Books.insert({title: "The Lord of the Rings", value: "The Lord of the Rings", author: "J. R. R. Tolkien"});
+     	Books.insert({title: "The Catcher in the Rye", value: "The Catcher in the Rye", author: "J. D. Salinger"});
+    	Books.insert({title: "The Great Gatsby", value: "The Great Gatsby", author: "F. Scott Fitzgerald"});
+	}
+}
+
+  Books._ensureIndex({
+      "value": "text"
+    });
+    seed();
 });
+
+Meteor.publish("search", function(searchValue) {
+	if(!searchValue) {
+		return Books.find({});
+	}
+	console.log("Searching for ", searchValue);
+	var cursor = Books.find(
+	{ $text: {$search: searchValue}	}, 
+	{
+		fields: {
+			score: { $meta: "textScore"}
+		},
+		sort: {
+			score: { $meta: "textScore"}
+		}
+	});
+	return cursor;
+});
+
+
+
