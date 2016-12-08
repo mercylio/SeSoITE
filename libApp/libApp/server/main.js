a5e198355577d79296d8de16eef18fe92b037abb
astan <AlexandruCristian.Stan@stud-inf.unibz.it>
2016-12-08 19:12:48 +0100

    8

--- libApp/libApp/server/main.js
+++ libApp/libApp/server/main.js
@@ -40,11 +40,15 @@ Meteor.publish("search", function(searchValue) {
 });
 
 Meteor.methods({
-	getCurrentTime: function (searchValue) {
-		console.log("Searching for ", searchValue);
-		//console.log('on server, getCurrentTime called');
+	getCurrentTime: function () {
+		console.log('on server, getCurrentTime called');
+	
+	//getCurrentTime: function (searchValue) {
+	//	console.log("Searching for ", searchValue);
+		
 		var convertAsyncToSync  = Meteor.wrapAsync( HTTP.get ),
-        result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q=any,contains,' + searchValue +'&apikey=XXXX', {} );
+    //    result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q=any,contains,' + searchValue +'&apikey=l7xx53f22519810d4f56a21caceb0fc95de4', {} );
+        result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q=any,contains,Hello&apikey=l7xx53f22519810d4f56a21caceb0fc95de4', {} );
         console.log(result.data.docs[0]);
         return result.data.docs;
 	}
