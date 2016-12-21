8599855b7b39ee56dd57aa2100a39181be60ee4e
astan <AlexandruCristian.Stan@stud-inf.unibz.it>
2016-12-21 21:32:40 +0100

    code cleanup and last little changes

--- libApp/libApp/client/main.js
+++ libApp/libApp/client/main.js
@@ -1,15 +1,13 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-//import { Session } from 'meteor/session'
-
-Books = new Mongo.Collection("books");
-
 import './main.html';
 
 
-//INFINITE SCROLL
+//Scroll List
 Session.set("bookListLimit", 10);
 lastScrollTop = 0;
+
+//Scrolling function
 $(window).scroll(function(event){
   //test if we're near the bottom of the window
   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
@@ -21,56 +19,46 @@ $(window).scroll(function(event){
       Session.set("bookListLimit", Session.get("bookListLimit") + 4);
     }
     lastScrollTop = scrollTop;
-
   }
 });
 
-Template.search.result = function () {
-  return Session.get('serverSimpleResponse');
-};
-
-Template.advancedsearch.result = function () {
-  return Session.get('serverSimpleResponse');
-};
-
-
-//SEARCH
-Template.search.events({
-  "submit #search": function (e) {
-      e.preventDefault();
-      NProgress.start();
-
-      //Meteor.call('getCurrentTime',function(err, response){ //IMPORTANT
-      Meteor.call('getSimpleSearch',Session.get("searchValue"),function(err, response) {
-        Session.set('serverSimpleResponse', response);
-        NProgress.done();
-      });
-      Session.set("searchValue", $("#searchValue").val());
-      NProgress.remove();
-    }
-}),
-    $(function(){
-      $("#myTable").tablesorter();
-    });
-;
-
-Template.search.helpers({
-
-});
-
+//Places the result in an array
 Handlebars.registerHelper('arrayify',function(obj){
     result = [];
     for (var key in obj) result.push(obj[key]);
     return result;
 });
 
+
+//SIMPLE SEARCH
+Template.search.events({
+  "submit #search": function (e) {
+      e.preventDefault();
+      NProgress.start();
+
+      Meteor.call('getSimpleSearch',Session.get("searchValue"),function(err, response) {
+        Session.set('serverSimpleResponse', response);
+        NProgress.done();
+      });
+
+      Session.set("searchValue", $("#searchValue").val());
+      NProgress.remove();
+    }
+}),
+    //sorting the table with a click on the column header
+    $(function(){
+      $("#myTable").tablesorter();
+    });
+;
+
+
 //ADVANCED SEARCH
 Template.advancedsearch.events({
   "submit #adv-search": function (e) {
       e.preventDefault();
       //NProgress.start();
-      //Meteor.call('getAdvancedResults',function(err, response){
       
+      //collecting the parameters for the advanced search
       var scopeOne = $("#scopeOne option:selected").val();
       var scopeTwo = $("#scopeTwo option:selected").val();
       var categoryOne = $("#categoryOne option:selected").val();
@@ -79,28 +67,26 @@ Template.advancedsearch.events({
       var searchValueOne = $("#searchValueOne").val();
       var searchValueTwo = $("#searchValueTwo").val();
 
-
-
       Meteor.call('getAdvancedResults',Session.get("scopeOne","categoryOne","searchValueOne"),function(err, response) {
-        //console.log(response);
+
         Session.set('serverSimpleResponse', response);
       });
       Session.set("scopeOne", $("#scopeOne").val(), "categoryOne", $("#categoryOne").val(), "searchValueOne", $("#searchValueOne").val());
       //NProgress.end();
     }
 }),
+    //sorting the table with a click on the column header
     $(function(){
       $("#myTable").tablesorter();
     });
 ;
 
-Template.advancedsearch.helpers({
-/*  books: function() {
-    Meteor.subscribe("adv-search", Session.get("searchFieldOne"));
-    if (Session.get("searchFieldOne")) {
-        return Books.find({}, { sort: [["score", "desc"]] });  
-      } else {
-        return Books.find({});
-      }
-    }*/
-});
+//retrieves the results of the simple search and loads them into the page
+Template.search.result = function () {
+  return Session.get('serverSimpleResponse');
+};
+
+//retrieves the results of the advanced search and loads them into the page
+Template.advancedsearch.result = function () {
+  return Session.get('serverSimpleResponse');
+};
--- libApp/libApp/server/main.js
+++ libApp/libApp/server/main.js
@@ -2,34 +2,23 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.methods({
 	
+	//simple search function: retrieves the list of items matching the word(s)
 	getSimpleSearch: function (searchValue) {
 		console.log("Searching for ", searchValue);
 		
 		var convertAsyncToSync  = Meteor.wrapAsync( HTTP.get ),
         result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q=any,contains,'+searchValue+'&apikey=l7xx53f22519810d4f56a21caceb0fc95de4', {} );
-	    //PREVIOUS EXAMPLE
-	    //    result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q=any,contains,Hello&apikey=l7xx53f22519810d4f56a21caceb0fc95de4', {} );
-	    //    console.log(result.data.docs[0]);
     	console.log("Simple Search Complete");
         return result.data.docs;
 	},
 
-	getAdvancedResults: function (scopeOne,categoryOne,searchValueOne) { /* (scopeOne,scopeTwo,categoryOne,categoryTwo,condition,searchValueOne,searchValueTwo) { */
-		console.log("Searching for ", scopeOne+"__"+categoryOne+"__"+searchValueOne/*+"_"+searchValueTwo*/);
-		 
+	//advanced search function: retrieves results filtered by various parameters
+	getAdvancedResults: function (scopeOne,categoryOne,searchValueOne) {
+		console.log("Searching for ", scopeOne+"__"+categoryOne+"__"+searchValueOne);
+
 		var convertAsyncToSync  = Meteor.wrapAsync( HTTP.get ),
-        result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q='+scopeOne+/*any*/','+categoryOne+/*contains*/','+searchValueOne+/*'&boolOperator='+condition+','+scopeTwo+','+categoryTwo+','+searchValueTwo+*/'&apikey=l7xx53f22519810d4f56a21caceb0fc95de4', {} );
-        //PREVIOUS EXAMPLE
-	    //    result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q=any,contains,Hello&apikey=l7xx53f22519810d4f56a21caceb0fc95de4', {} );
-	    //    console.log(result.data.docs[0]);
+        result = convertAsyncToSync( 'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q='+scopeOne+','+categoryOne+','+searchValueOne+',&apikey=l7xx53f22519810d4f56a21caceb0fc95de4', {} );
     	console.log("Advanced Search Complete");
         return result.data.docs;
 	}
-
 });
-
-
-//Meteor.http.call("GET","https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?vid=UNIBZ&scope=All&q=any,contains,Hello&apikey=XXXXX",function(error,result){
-//        console.log(result.data.docs[0]);
-//        //return result.data.docs[0];
-//    });
